1e32ad22145f7ed6b4f762ceb0aff6751549fba7
红血球AE3803 <2544390577@qq.com>
2020-08-15 23:55:50 +0800

    Add files via upload

--- inc/classes/database.php
+++ inc/classes/database.php
@@ -45,7 +45,7 @@ class database {
     }
     //检测用户名是否存在//
     function isUserExist($username) {
-        $res = $this->query("select * from pre_ucenter_members where username = '".$username."'");
+        $res = $this->query("select * from members where username = '".$username."'");
         if (!$res) {
             return false;
         } else {
@@ -54,7 +54,7 @@ class database {
     }
     //检测邮箱是否存在//
     function isEmailExist($email) {
-        $res = $this->query("select * from pre_ucenter_members where email = '".$email."'");
+        $res = $this->query("select * from members where email = '".$email."'");
         if (!$res) {
             return false;
         } else {
@@ -63,20 +63,20 @@ class database {
     }
     //将注册数据写入数据库//
     function creUser($username,$password,$email,$regip,$salt,$userid,$uuid) {
-        $this->query_change("insert into pre_ucenter_members (username, password, email, regip, regdate, salt, userid, uuid) values ('".$username."', '".$password."', '".$email."', '".$regip."', '".time()."', '".$salt."', '".$userid."', '".$uuid."')");
+        $this->query_change("insert into members (username, password, email, regip, regdate, salt, userid, uuid) values ('".$username."', '".$password."', '".$email."', '".$regip."', '".time()."', '".$salt."', '".$userid."', '".$uuid."')");
     }
     //创建正版用户数据//
     function creMojangUser($username,$password,$email,$regip,$salt,$userid,$uuid,$skin){
-        $res = $this->query("select email from pre_ucenter_members where email = '".$email."'");
+        $res = $this->query("select email from members where email = '".$email."'");
         if(!$res[0][0]){
-            $this->query_change("insert into pre_ucenter_members (username, password, email, regip, regdate, salt, userid, uuid, texturedata, mojang) values ('".$username."', '".$password."', '".$email."', '".$regip."', '".time()."', '".$salt."', '".$userid."', '".$uuid."', '".$skin."', 'true')");
+            $this->query_change("insert into members (username, password, email, regip, regdate, salt, userid, uuid, texturedata, mojang) values ('".$username."', '".$password."', '".$email."', '".$regip."', '".time()."', '".$salt."', '".$userid."', '".$uuid."', '".$skin."', 'true')");
         }else{
-            $this->query_change("update pre_ucenter_members set username = '".$username."', password= '".$password."', regip= '".$regip."', regdate = '".time()."', salt = '".$salt."', userid = '".$userid."', uuid = '".$uuid."', texturedata = '".$skin."', mojang = 'true' where email = '".$email."'");
+            $this->query_change("update members set username = '".$username."', password= '".$password."', regip= '".$regip."', regdate = '".time()."', salt = '".$salt."', userid = '".$userid."', uuid = '".$uuid."', texturedata = '".$skin."', mojang = 'true' where email = '".$email."'");
         }
     }
     //验证是否已认证过//
     function isVerified($uuid){
-        $res = $this->query("select * from pre_ucenter_members where uuid = '".$uuid."'");
+        $res = $this->query("select * from members where uuid = '".$uuid."'");
         $verified = $res[0][16];
         if($verified == 'true'){
             return true;
